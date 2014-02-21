d295ec7d417ec04b8fe950cfdc83d91a501124d7
MrRoundTree <mrroundtree@mrroundtree-HP-Mini-210-1000.(none)>
2014-02-21 23:19:00 +0530

    Changes to app_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,16 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "WorkDay Scheduler"
     if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
     end
-  end
+end
+
+	def image_path
+	image_path ="app/assets/images/wds_small.jpg"
+	end
+ 
 end

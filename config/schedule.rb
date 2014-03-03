eea6ad501ee50bce5a8c33bcaee9a65d3315c049
MrRoundTree <mrroundtree@mrroundtree-HP-Mini-210-1000.(none)>
2014-03-03 22:42:19 +0530

    cronjob changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
-set :environment,"development"
+set :environment,"production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 4.minutes do
+every 8.hours do
   rake "db:updatewh"
 end
 
